d39af0e2eefcbba44da5d35f651f6ed96b44300c
keerthana1001 <51878842+keerthana1001@users.noreply.github.com>
2020-03-04 15:48:34 +0530

    2D Game

---
+++ 2dfirst/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector2 targetPosition;
+    private float incY = 2.4f;
+    public float MaxHeight;
+    public float MinHeight;
+    public float Speed;
+    public float health = 3;
+    public Text HealthText;
+    public GameObject panel;
+    private void Update()
+    {
+        if(health<=0)
+        {
+            panel.SetActive(true);
+            Debug.Log("gameOver");
+            gameObject.SetActive(false);
+        }
+        if(Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y<MaxHeight)
+        {
+            //Debug.Log("UpArrow Pressed");
+            targetPosition = new Vector2(transform.position.x, transform.position.y + incY);
+            //Debug.Log(transform.position);
+
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y>MinHeight)
+        {
+            //Debug.Log("DownArrow Pressed");
+            targetPosition = new Vector2(transform.position.x, transform.position.y - incY);
+           
+            //Debug.Log(transform.position);
+        }
+
+        HealthText.text = health.ToString();
+        transform.position = Vector2.MoveTowards(transform.position, targetPosition, Speed * Time.deltaTime);
+       
+        
+    }
+    public void goUp()
+    {
+        if (transform.position.y < MaxHeight)
+        {
+            targetPosition = new Vector2(transform.position.x, transform.position.y + incY);
+        }
+    }
+    public void goDown()
+    { 
+        if (transform.position.y > MinHeight)
+        {
+            targetPosition = new Vector2(transform.position.x, transform.position.y - incY);
+        }
+    }
+    public void restart()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+        SceneManager.LoadScene("SampleScene");
+    }
+}
---
+++ 2dfirst/Assets/Scripts/Spawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    
+    public GameObject[] obstaclePatterns;
+    private float timeBtwspawn;
+    public float startTimeBtwSpawn;
+    public float decreaseTime;
+    public float minTime = 0.65f;
+     
+    private void Update()
+    {
+      if(timeBtwspawn <= 0)
+        {
+            int rand = Random.Range(0, obstaclePatterns.Length);
+            Instantiate(obstaclePatterns[rand], transform.position, Quaternion.identity);
+            timeBtwspawn = startTimeBtwSpawn;
+            if (startTimeBtwSpawn > minTime)
+            {
+
+                startTimeBtwSpawn -= decreaseTime;
+            }
+        }
+        else
+        {
+            timeBtwspawn -= Time.deltaTime;
+        }
+    }
+}
